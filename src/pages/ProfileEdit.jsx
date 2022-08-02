2cf3b7adb018f70bf2cc67399bbae11825a61d9a
Hugo Leonardo <huleonardo19@gmail.com>
2022-08-02 17:39:57 -0300

    req 14

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,14 +1,143 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { getUser, updateUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class ProfileEdit extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+      isDisabled: true,
+    };
+  }
+
+  async componentDidMount() {
+    const userObj = await getUser();
+    this.setState({ loading: false,
+      name: userObj.name,
+      email: userObj.email,
+      image: userObj.image,
+      description: userObj.description }, () => this.validateButton());
+  }
+
+  validateButton = () => {
+    const { name, email, image, description } = this.state;
+    const boolLength = name.length > 0
+      && email.length > 0 && image.length > 0 && description.length > 0;
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    const checkEmail = email.match(pattern);
+    const finalCheck = boolLength && checkEmail;
+    this.setState({ isDisabled: !finalCheck });
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      this.validateButton();
+    });
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({ loading: true }, async () => {
+      const { name, email, image, description } = this.state;
+      const userObj = {
+        name,
+        email,
+        image,
+        description,
+      };
+      await updateUser(userObj);
+      const { history } = this.props;
+      history.push('/profile');
+    });
+  }
+
   render() {
+    const { loading, name, email, image, description, isDisabled } = this.state;
+    if (loading) {
+      return (
+        <div data-testid="page-profile-edit">
+          <Header />
+          ProfileEdit
+          <Loading />
+
+        </div>
+      );
+    }
     return (
       <div data-testid="page-profile-edit">
         <Header />
         ProfileEdit
+        <form>
+          <label htmlFor="input-name">
+            Nome:
+            <input
+              id="input-name"
+              data-testid="edit-input-name"
+              name="name"
+              value={ name }
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="input-email">
+            Email:
+            <input
+              id="input-email"
+              data-testid="edit-input-email"
+              name="email"
+              value={ email }
+              type="email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="input-description">
+            Description:
+            <input
+              id="input-description"
+              data-testid="edit-input-description"
+              name="description"
+              value={ description }
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="input-image">
+            Imagem:
+            <input
+              id="input-image"
+              data-testid="edit-input-image"
+              name="image"
+              value={ image }
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="edit-button-save"
+            type="button"
+            disabled={ isDisabled }
+            onClick={ this.handleClick }
+          >
+            Enviar
+
+          </button>
+        </form>
 
       </div>
     );
   }
 }
+
+ProfileEdit.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
